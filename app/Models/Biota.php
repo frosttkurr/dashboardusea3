1e94de4450bd647efacb44081dcfeed036d16c5b
frosttkurr <thisiscyber13@gmail.com>
2023-08-15 04:32:11 +0800

    add softdeletes for lokasis

--- app/Models/Biota.php
+++ app/Models/Biota.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Biota extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     public function jenisBiota(){
         return $this->hasOne(JenisBiota::class, 'id', 'id_jenis_biota');
--- database/migrations/2023_02_18_093600_create_lokasis_table.php
+++ database/migrations/2023_02_18_093600_create_lokasis_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('latitude');
             $table->string('longitude');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
